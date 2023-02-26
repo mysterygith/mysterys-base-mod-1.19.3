4004e31f3df6bc369217f656457a5b1c6e56590f
mysterygith <littlespaz1104@gmail.com>
2023-02-26 10:59:23 -0500

    Initial commit

---
+++ src/main/java/mystery/mbase/MBaseClient.java
@@ -0,0 +1,10 @@
+package mystery.mbase;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MBaseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
