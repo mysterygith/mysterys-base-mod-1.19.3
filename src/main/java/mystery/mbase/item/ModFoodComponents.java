d8876b9b297ea4557c7f6b77558d31712778d1f2
mysterygith <littlespaz1104@gmail.com>
2023-03-11 13:15:33 -0500

    Food
    -banana
    -banana bread
    
    Steel Changes
    
    -pig iron
    -steel smelting

---
+++ src/main/java/mystery/mbase/item/ModFoodComponents.java
@@ -0,0 +1,11 @@
+package mystery.mbase.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+
+    public static final FoodComponent BANANA = new FoodComponent.Builder().hunger(4).saturationModifier(0.3f).build();
+    public static final FoodComponent BANANA_BREAD = new FoodComponent.Builder().hunger(6).saturationModifier(0.5f).build();
+
+
+}
--- src/main/java/mystery/mbase/item/ModItems.java
+++ src/main/java/mystery/mbase/item/ModItems.java
@@ -3,9 +3,7 @@ package mystery.mbase.item;
 import mystery.mbase.MBase;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -13,6 +11,15 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item STEEL_INGOT = registerItem("steel_ingot",
             new Item(new FabricItemSettings()));
+
+    public static final Item PIG_IRON = registerItem("pig_iron",
+            new Item(new FabricItemSettings()));
+
+    public static final Item BANANA_BREAD = registerItem("banana_bread",
+            new Item(new FabricItemSettings().food(ModFoodComponents.BANANA_BREAD)));
+
+    public static final Item BANANA = registerItem("banana",
+            new Item(new FabricItemSettings().food(ModFoodComponents.BANANA)));
 
 
 
@@ -22,6 +29,9 @@ public class ModItems {
 
     public static void addItemsToItemGroup() {
         addToItemGroup(ItemGroups.INGREDIENTS, STEEL_INGOT);
+        addToItemGroup(ItemGroups.INGREDIENTS, PIG_IRON);
+        addToItemGroup(ItemGroups.FOOD_AND_DRINK, BANANA);
+        addToItemGroup(ItemGroups.FOOD_AND_DRINK, BANANA_BREAD);
 
 
 
